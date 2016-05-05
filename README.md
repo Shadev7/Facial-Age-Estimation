cd315760ae8b61e4d87b4a25de4372bccf044208
Srivatsan Iyer <srriyer@indiana.edu>
2016-05-04 23:54:28 -0400

    Adding report, update README. Remove main.py

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Age and Gender Estimation using images of faces 
+# Age Estimation using Images of faces 
+
+The program in the program is configured to run against all combinations of the classifiers, the feature models, age groups. It will print out a confusion matrix.
+
+To run the program, do:
+
+$ python main.py
+
+
--- main.py
+++ main.py
@@ -1,183 +1,98 @@
 import sys
-import numpy as np
-import cv2
-from collections import namedtuple
+import glob
+import os
+from itertools import chain
 
-from FaceDetector import FaceDetector
-from CustomFilter import create_eye_left_filter
+import dlib
+import cv2
+from skimage import io
+
+from core.inputreader import MugshotExtractor, DataTangExtractor
+from core.classifier import *
+from core.featureconverter import *
+from core.model import AgeBucket
+import config
+from utils import Tee
+
+
+def train_test(train, test, classifier):
+    classifier.train(train.list_data())
+    res = classifier.test(test.list_data())
+    
+    confusion_matrix = [[0]*classifier.converter.n_output 
+            for _ in range(classifier.converter.n_output)]
+
+    for index, (predicted, _, correct) in enumerate(zip(*res)):
+    	if predicted != correct:
+    		# print "incorrect detection:", test.list_data()[index]
+    		pass
+        confusion_matrix[correct][predicted] += 1
+
+    print "Confusion Matrix:"
+    print "\n".join(" ".join("%4d"%x for x in row) for row in confusion_matrix)
+
+    correct = sum(confusion_matrix[i][i] for i in range(len(confusion_matrix)))
+    total = len(res[0])
+    print "Overall Accuracy:", correct / float(total) *100, "%", 
+    print "(%d out of %d)"%(correct, total)
+    return correct, total, correct/float(total)
+
+def run(cls, classifier_param):
+    train = DataTangExtractor("data/datatang/train")
+    test = DataTangExtractor("data/datatang/test")
+
+    repeat = 2
+    total, totalAccuracy = 0, 0
+    for i in range(repeat):
+        classifier = cls(*classifier_param)
+
+        print "Trial #", i + 1
+
+        correct, len_td, accuracy = train_test(train, test, classifier)
+        total += correct
+        totalAccuracy += accuracy
+    return (total/float(repeat), totalAccuracy/float(repeat), 
+                totalAccuracy/float(repeat) * classifier.converter.n_output)
 
 def main():
-#     input image
-    img_name = "face1.png"
-    #img_name = "face1.png"
-    #img_name = "data/28754132@N06/landmark_aligned_face.608.9691370454_849ce3fb06_o.jpg"
-    input_img = cv2.imread(img_name)
-    height, width, channels = input_img.shape
-    #print "input image shape: ", input_img.shape
-    aspect_ratio = 1.0 * width / height
-    #print "input image aspect_ratio: ", aspect_ratio
+    stdout = sys.stdout
+    f = open('stdout.txt', 'w')
+    sys.stdout = Tee(sys.stdout, f)
+    classifiers = [
+        (ScikitNeuralNetClassifier, [[0, 10, 0]]),
+        (ScikitNeuralNetClassifier, [[0, 15, 0]]),
+        (ScikitNeuralNetClassifier, [[0, 30, 0]]),
+        (ScikitNeuralNetClassifier, [[0, 50, 0]]),
+        (ScikitNeuralNetClassifier, [[0, 100,0]]),
+        (ScikitNaiveBayesClassifier, []),
+        (NearestNeighborsClassifier, [5]),
+        (NearestNeighborsClassifier, [10]),
+        (NearestNeighborsClassifier, [15]),
+    ]
+    features = [
+        FaceLandmarkFeatureConverter,
+        FaceBoundaryFeatureConverter,
+        FaceLandmarkBoundaryFeatureConverter,
+    ]
+    age_groups = [
+        (12, 23, 100),
+        (7, 15 ,22, 100),
+        (5, 12, 18, 100),
+        (5, 10, 15, 20, 25,  100),
+    ]
 
-    FaceAttrib = namedtuple("FaceAttrib", "n en ex ch al") #as per the face annotation diagram
-    
-    
-#     resize image
-    new_width = 400
-    new_height = int(new_width / aspect_ratio)
-    resized_img = cv2.resize(input_img, (new_width, new_height)) 
-    #print "resized image shape: ", resized_img.shape
-    #cv2.imshow("Resized Image", resized_img)
- 
-#     detect face
-    face_cascade = cv2.CascadeClassifier('haar_models/haarcascade_frontalface_default.xml')
-    #face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haar_models/haarcascade_eye.xml')
-    mouth_cascade = cv2.CascadeClassifier('haar-models/Mouth.xml')
-    nose_cascade = cv2.CascadeClassifier('haar-models/Nariz.xml')
-    #eye_cascade = cv2.CascadeClassifier('parojosG.xml')
-    nose_cascade2 = cv2.CascadeClassifier('haar-models/Nariz_nuevo_20stages.xml')
-    
-    gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1, #compensate closeness to the camera
-        minNeighbors=5, #show many objects are detected near the current one before it declares the face found
-        minSize=(30, 30), #size of each window
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
-    for (x, y, w, h) in faces:
-        cv2.rectangle(resized_img, (x, y), (x + w, y + h), (255, 0, 0), 2) #face rectangle
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = resized_img[y:y + h, x:x + w]
-#     detect eyes 
-        eyes = eye_cascade.detectMultiScale(
-            roi_gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-        )
-        i=0
-        FaceAttrib.ex = (eyes[0][0] + eyes[0][2]) - eyes[1][0] # distance between eyes extreme ends x-cord
-        FaceAttrib.en =  eyes[0][0] - (eyes[1][0] + eyes[1][2]) # distance between eyes near ends x-cord
-        FaceAttrib.n =  eyes[0][0] + FaceAttrib.ex/2 #point between eyes y-cord
-        print "ex : ", FaceAttrib.ex
-        print  "en : ", FaceAttrib.en
-        print "Orbital width index :", str(FaceAttrib.ex/FaceAttrib.en)
-        print  "n : ", FaceAttrib.n
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-            roi_eye_gray = roi_gray[ey:ey + eh, ex:ex + ew]
-            smoothed = cv2.GaussianBlur(roi_eye_gray, (3,3), 30)
-            cannied = cv2.Canny(smoothed, 50,100 )
-            gftt = cv2.goodFeaturesToTrack (roi_eye_gray, 20, 0.01,10)
-
-	    #cv2.imshow("GFTT", gftt)
-            #cv2.imshow("Canny", cannied)
-	    p = gftt > 0.01*gftt.max()
-            roi_color[ex,ey] = [0, 0, 255]
-	    for i in gftt:
-	        #print (i[0][0])
-		cv2.circle(roi_color, (ex+ int(i[0][0]), ey+int(i[0][1])), 2, (0, 255, 255), 1)
-            '''for i in range(gftt.shape[0]):
-                for j in range(gftt.shape[1]):
-		    print "gftt"
-		    print gftt[i,j]
-		    cv2.circle(roi_color, (ex+i, ey+j), 2, (0, 255, 0), 1)
-                    if(gftt[i,j] > 0.01*gftt.max()):
-                        cv2.circle(roi_color, (ex+i, ey+j), 2, (0, 255, 0), 1)'''
-
-            temp_kernel = np.ones((3,3), np.uint8)
-            dilated = cv2.dilate(cannied, temp_kernel, iterations=1)
-            cv2.imshow("processed", cv2.erode(dilated, temp_kernel, iterations=1))
-            #filt = create_eye_left_filter(100)
-            for i in range(2,15):
-                res = cv2.filter2D(dilated, cv2.CV_32F, create_eye_left_filter(i))
-                #cv2.imshow(str(i), res)
-            print "Min:", res.min(), "| Max:", res.max()
-            res = (res - res.min()) * (255 / (res.max() - res.min()))
-            print "Min:", res.min(), "| Max:", res.max()
-            print res
-            #cv2.waitKey(0)
-            #exit()
-
-
-
-#     detect nose 
-        nose = nose_cascade.detectMultiScale(
-            roi_gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-        )
-        i=0
-        #n_x = 0
-        n_y = 0
-        for (nx, ny, nw, nh) in nose:
-            cv2.rectangle(roi_color, (nx, ny), (nx + nw, ny + nh), (0, 0, 100), 2)
-            #print "rect: ", ex, ey
-            print "w,h: ", nw, nh
-            roi_gray2 = roi_gray[ny:ny + nh, nx:nx + nw]
-#             cv2.imshow("Detected Eye Corners"+str(i), roi_gray2)
-            i=i+1
-            n_y = ny + nh +nh
-
-#     detect nose2 
-        nose = nose_cascade2.detectMultiScale(
-            roi_gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-        )
-        i=0
-        #n_x = 0
-        n_y = 0
-        for (nx, ny, nw, nh) in nose:
-            cv2.rectangle(roi_color, (nx, ny), (nx + nw, ny + nh), (0, 100, 100), 2)
-            #print "rect: ", ex, ey
-            print "w,h: ", nw, nh
-            roi_gray2 = roi_gray[ny:ny + nh, nx:nx + nw]
-#             cv2.imshow("Detected Eye Corners"+str(i), roi_gray2)
-            i=i+1
-            n_y = ny + nh +nh
-
-#     detect mouth
-        roi_gray_m = gray[n_y:y + h, x:x + w]
-        roi_color_m = resized_img[n_y:y + h, x:x + w]
-        mouth = mouth_cascade.detectMultiScale(
-            roi_gray_m,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-        )
-        for (mx, my, mw, mh) in mouth:
-            cv2.rectangle(roi_color_m, (mx, my), (mx + mw, my + mh), (100, 100, 0), 2)
-            #print "rect: ", ex, ey
-	    #print "w,h: ", ew, eh
-            roi_gray2 = roi_gray_m[my:my + mh, mx:mx + mw]
-#             cv2.imshow("Detected Eye Corners"+str(i), roi_gray2)
-            #detect corner of mouth
-            '''dst = cv2.cornerHarris(roi_gray2,2,3,0.04)
-            dst = cv2.dilate(dst, None)
-            print dst.shape
-            p = dst > 0.01*dst.max()
-            roi_color[ex,ey] = [0, 0, 255]
-            for i in range(dst.shape[0]):
-                for j in range(dst.shape[1]):
-                    if(dst[i,j] > 0.01*dst.max()):
-                        cv2.circle(roi_color, (ex+i, ey+j), 2, (0, 255, 0), 1)'''            
-
-    cv2.imshow("Detected Face and Eyes", resized_img)
-    
-#     cv2.imshow("Detected Eye Corners", resized_img)
-    
-#==================================================================================
-#==================================================================================    
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    exit()
-
+    for cls, param in classifiers:
+        for ftr in features:
+            for ag in age_groups:
+                print "Classifier:", cls
+                print "Classifier param:", param
+                print "Feature:", ftr
+                print "Age Group:", ag
+                res = run(cls, [ftr(AgeBucket(*ag))] + param)
+                print "Avg. Correct:", res[0]
+                print "Avg. Accuracy:", res[1]*100, "%"
+                print "BTRG:", res[2], "times"
+                sys.stdout.flush()
+    f.close()
 if __name__ == '__main__':
     main()
